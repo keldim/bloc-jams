1f87769ac34ab08d7e00a3cf6ae9df099c4ccc10
Chris (Won Seok) Yoo <woyoo@umich.edu>
2017-01-09 09:43:44 +0900

    finish assignment-9-domscripting-events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,25 +1,18 @@
 
 var pointsArray = document.getElementsByClassName('point');
  
- var animatePoints = function(points) {
-
-    
-                 var revealPoint = function (num1) {
+ var revealPoint = function (point) {
                         
-                        points[num1].style.opacity = 1;
-                        points[num1].style.transform = "scaleX(1) translateY(0)";
-                        points[num1].style.msTransform = "scaleX(1) translateY(0)";
-                        points[num1].style.WebkitTransform = "scaleX(1) translateY(0)";
+                        point.style.opacity = 1;
+                        point.style.transform = "scaleX(1) translateY(0)";
+                        point.style.msTransform = "scaleX(1) translateY(0)";
+                        point.style.WebkitTransform = "scaleX(1) translateY(0)";
                     
-                };
-    
-    for(var i=0; i < points.length; i++) {
-                        revealPoint(i);
-                    }
-    
-                 
+                }; 
 
+var animatePoints = function(points) {
 
+    forEach(points, revealPoint);
  };
  
  window.onload = function() {
