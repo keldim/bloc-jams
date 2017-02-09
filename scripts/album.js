6931074e393fd5486134d93ad6acb3fa0b564dc0
Chris (Won Seok) Yoo <woyoo@umich.edu>
2017-02-09 12:48:19 +0900

    finsih checkpoint-20-jquery

--- scripts/album.js
+++ scripts/album.js
@@ -1,7 +1,25 @@
 var setSong = function (songNumber) {
-  currentlyPlayingSongNumber = parseInt(songNumber);
+   if (currentSoundFile) {
+         currentSoundFile.stop();
+     }
+    
+    currentlyPlayingSongNumber = parseInt(songNumber);
   currentSongFromAlbum = currentAlbum.songs[songNumber-1];
+
+ currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+         formats: [ 'mp3' ],
+         preload: true
+     });
+    
+    setVolume(currentVolume);
 };
+
+var setVolume = function(volume) {
+     if (currentSoundFile) {
+         currentSoundFile.setVolume(volume);
+     }
+ };
+
 
 var getSongNumberCell = function (number) {
     return $('.song-item-number[data-song-number="' + number + '"]');
@@ -30,14 +48,22 @@ var createSongRow = function(songNumber, songName, songLength) {
 		// Switch from Play -> Pause button to indicate new song is playing.
 		$(this).html(pauseButtonTemplate);
 		setSong(songNumber);
+         currentSoundFile.play();
           updatePlayerBarSong();
 	} else if (currentlyPlayingSongNumber === songNumber) {
 		// Switch from Pause -> Play button to pause currently playing song.
-		$(this).html(playButtonTemplate);
+		if (currentSoundFile.isPasued()) {
+             $(this).html(pauseButtonTemplate);
+		$('.main-controls .play-pause').html(playerBarPauseButton);
+             currentSoundFile.play();
+        }
+        
+        else {
+            $(this).html(playButtonTemplate);
 		$('.main-controls .play-pause').html(playerBarPlayButton);
-        currentlyPlayingSongNumber = null;
-        currentSongFromAlbum = null;
-	}
+            currentSoundFile.pause();  
+        }
+        }
      };
     
      var onHover = function(event) {
@@ -115,6 +141,7 @@ var nextSong = function() {
     
     // Set a new current song
     setSong(currentSongIndex + 1);
+      currentSoundFile.play();
 
     // Update the Player Bar information
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
@@ -150,6 +177,7 @@ var previousSong = function() {
     
     // Set a new current song
    setSong(currentSongIndex + 1);
+      currentSoundFile.play();
 
     // Update the Player Bar information
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
@@ -192,7 +220,8 @@ var $previousButton = $('.main-controls .previous');
 
 var currentlyPlayingSongNumber = null;
  var currentSongFromAlbum = null;
-
+var currentSoundFile = null;
+var currentVolume = 80;
 
  $(document).ready(function() {
      setCurrentAlbum(albumPicasso);
